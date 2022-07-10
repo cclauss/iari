b28db63385403a1395f042d2fafb2c6ebc34e0af
Christian Clauss <cclauss@me.com>
2022-07-10 16:34:11 +0200

    pipx install wcdimportbot

---
+++ src/wcdimportbot/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import logging
+
+try:
+    from ... import config
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        "config.py not found. Please follow the instructions in the README about how to set up the config.py file"
+    )
+from . import WcdImportBot
+
+def main() -> None:
+    logging.basicConfig(level=config.loglevel)
+    wcdimportbot = WcdImportBot()
+    wcdimportbot.run()
+
+if __name__ == "__main__":
+    main()
